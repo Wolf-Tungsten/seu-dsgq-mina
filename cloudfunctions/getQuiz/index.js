03cb105cac59b26b88da6e39dd034a44c646a1f4
Wolf-Tungsten <canis.tungsten@foxmail.com>
2018-11-17 00:51:39 +0800

    出题云函数搞定辽

---
+++ cloudfunctions/getQuiz/index.js
@@ -0,0 +1,40 @@
+// 云函数入口文件
+const cloud = require('wx-server-sdk')
+
+cloud.init()
+const db = cloud.database()
+
+// 配置
+const GQ_EASY = 10
+const GQ_DIFFICULT = 5
+const DS_EASY = 10
+const DS_DIFFICULT = 5
+
+// 云函数入口函数
+exports.main = async (event, context) => {
+    const dsEasyCount = (await db.collection('quiz').where({catalog:'党史', difficulty:'容易'}).count()).total
+    const dsDifficultCount = (await db.collection('quiz').where({catalog:'党史', difficulty:'困难'}).count()).total
+    const gqEasyCount = (await db.collection('quiz').where({catalog:'国情', difficulty:'容易'}).count()).total
+    const gqDifficultCount = (await db.collection('quiz').where({catalog:'国情', difficulty:'困难'}).count()).total
+
+    let quizList = []
+
+
+    ;[[GQ_EASY, gqEasyCount, '国情', '容易'], 
+    [DS_EASY, dsEasyCount, '党史', '容易'], 
+    [GQ_DIFFICULT, gqDifficultCount, '国情', '困难'], 
+    [DS_DIFFICULT, dsDifficultCount, '党史', '困难']].forEach(k => {
+        for (let i = 0; i < k[0]; i++) {
+            quizList.push(
+                db.collection('quiz').where({catalog:k[2], difficulty:k[3]})
+                .skip(Math.round(Math.random()*k[1])).limit(1).get()
+            )
+        }
+    });
+
+    quizList = await Promise.all(quizList)
+
+    return quizList.map(k => {
+        return k.data[0]
+    })
+}
